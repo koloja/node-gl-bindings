8b6fe356aa2ead6f6845aa33d3289d5ef0c11821
koloja <dave.folling@gmail.com>
2024-10-24 17:27:23 +0100

    add more functions and values + comments

--- src/lib/opengl.ts
+++ src/lib/opengl.ts
@@ -4,20 +4,31 @@ import OpenGL from '../types/OpenGL';
 import path from 'node:path';
 
 const lib = ffi.Library(path.join(__dirname, '../../.dll/opengl32.dll'), {
+    // clearing buffers
     'glClearColor': ['void', ['float', 'float', 'float', 'float']],
     'glClear': ['void', ['int']],
+
+    // drawing primitives
     'glBegin': ['void', ['int']],
     'glEnd': ['void', []],
     'glVertex2f': ['void', ['float', 'float']],
     'glColor3f': ['void', ['float', 'float', 'float']],
+    
+    // setting viewport
     'glViewport': ['void', ['int', 'int', 'int', 'int']],
+    
+    // matrix operations
     'glMatrixMode': ['void', ['int']],
     'glLoadIdentity': ['void', []],
+    'glTranslatef': ['void', ['float', 'float', 'float']], // Importing glTranslatef for translations
     'glOrtho': ['void', ['double', 'double', 'double', 'double', 'double', 'double']],
-    'glFrustum': ['void', ['double', 'double', 'double', 'double', 'double', 'double']],
+    
+    // toggle capabilities
     'glEnable': ['void', ['int']],
     'glDisable': ['void', ['int']],
-    'glFlush': ['void', []]
+    
+    // misc
+    'glFlush': ['void', []],
 });
 
 const opengl = map(lib) as OpenGL;
--- src/types/OpenGL.ts
+++ src/types/OpenGL.ts
@@ -13,6 +13,15 @@ type OpenGL = {
     enable(cap: number): void;
     disable(cap: number): void;
     flush(): void;
+    translatef(x: number, y: number, z: number): void;
+    genTextures(n: number, textures: number[]): void;
+    bindTexture(target: number, texture: number): void;
+    texImage2D(target: number, level: number, internalformat: number, width: number, height: number, border: number, format: number, type: number, pixels: number | null): void;
+    texParameteri(target: number, pname: number, param: number): void;
+    drawArrays(mode: number, first: number, count: number): void;
+    drawElements(mode: number, count: number, type: number, indices: number | null): void;
+    pushMatrix(): void;
+    popMatrix(): void;
 };
 
 export default OpenGL;
